86d7368faa77e84541be4114375d062857851430
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-09-18 14:36:02 -0400

    add unclick functionality

--- scritpt.js
+++ scritpt.js
@@ -1,7 +1,8 @@
-const { Observable, fromEvent, operators} = rxjs;
+const { Observable, fromEvent, operators, EMPTY} = rxjs;
 const {map, switchMap, takeUntil, take} = operators;
 
 const myButton = document.getElementById('da-button');
+var clicked = false;
 const btnClick$ = fromEvent(myButton, 'click');
 const mouseMove$ = fromEvent(document, 'mousemove');
 
@@ -14,10 +15,10 @@ const observer = {
 };
 btnClick$.pipe(
   map((event) => {
-    console.log(event)
-    console.log(`Button clicked x-position: ${event.clientX}, y-position: ${event.clientY}`)
+    clicked = !clicked
+    console.log(`Button ${clicked? `clicked x-position: ${event.clientX}, y-position: ${event.clientY}` : 'unclicked'}`)
   }),
-  switchMap(() => mouseMove$),
+  switchMap(() => clicked? mouseMove$: EMPTY),
   map((event) => ({x: event.clientX, y: event.clientY})),
 
 ).subscribe(observer);
